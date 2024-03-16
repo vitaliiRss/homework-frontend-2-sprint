f288a623008294c15bd0e4a5bf0555f8f9ae956c
Vitalii <vitalii.hryshko.rss@gmail.com>
2024-03-16 10:49:39 +0100

    fix: remove extra condition whitch is broke test

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -46,11 +46,9 @@ const Greeting: React.FC<GreetingPropsType> = (
             onKeyDown={onEnter}
             onBlur={onBlur}
           />
-          {error && (
-            <div id={'hw3-error'} className={s.error}>
-              {error}
-            </div>
-          )}
+          <div id={'hw3-error'} className={s.error}>
+            {error}
+          </div>
         </div>
 
         <button
