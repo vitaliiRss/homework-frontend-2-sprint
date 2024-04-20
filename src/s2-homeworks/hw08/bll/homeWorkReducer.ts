9d3d2ac1a82711f275a1becc99e93ea869a30267
Vitalii <vitalii.hryshko.rss@gmail.com>
2024-04-20 15:34:46 +0200

    add: sprint 3 week 4

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,22 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8'
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: 'sort'; payload: 'up' | 'down' }
+  | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
+  switch (action.type) {
+    case 'sort': { // by name
+      if (action.payload === "up") {
+        return [...state].sort((user1, user2) => user1.name > user2.name ? 1 : -1) // need to fix
+      } else {
+        return [...state].sort((user1, user2) => user1.name < user2.name ? 1 : -1) // need to fix
+      }
     }
+    case 'check': {
+      return state.filter(user => user.age > action.payload) // need to fix
+    }
+    default:
+      return state
+  }
 }
