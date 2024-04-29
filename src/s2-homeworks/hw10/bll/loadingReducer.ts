b82d0ac15240bf5988af66cc4784d5b0a0a512f6
Vitalii <vitalii.hryshko.rss@gmail.com>
2024-04-29 21:10:14 +0200

    add: sprint 4 week 2

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,22 +1,24 @@
 const initState = {
-    isLoading: false,
+  isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // пишет студент  // need to fix
-
-        default:
-            return state
+export const loadingReducer = (state = initState, action: LoadingActionType): { isLoading: boolean } => { // fix any
+  switch (action.type) {
+    // пишет студент  // need to fix
+    case "CHANGE_LOADING": {
+      return { ...state, isLoading: action.isLoading }
     }
+    default:
+      return state
+  }
 }
 
 type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
+  type: 'CHANGE_LOADING'
+  isLoading: boolean
 }
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
-    type: 'CHANGE_LOADING',
-    isLoading,
+  type: 'CHANGE_LOADING',
+  isLoading,
 })
