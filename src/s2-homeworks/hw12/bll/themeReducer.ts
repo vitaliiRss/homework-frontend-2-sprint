b0ad27978d1d35ba3be5d57b282e5706e968eabb
Vitalii <vitalii.hryshko.rss@gmail.com>
2024-05-14 17:54:04 +0200

    add: sprint 4 week 4

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,28 @@
+type ThemeStatetype = {
+  themeId: number
+}
+
 const initState = {
-    themeId: 1,
+  themeId: 1
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
-
-        default:
-            return state
+export const themeReducer = (state = initState, action: ThemeActionsType): ThemeStatetype => {
+  switch (action.type) {
+    case "SET_THEME_ID": {
+      return { ...state, themeId: action.id }
     }
+    default:
+      return state
+  }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type ThemeActionsType = ChangeThemeIdType
+
+type ChangeThemeIdType = ReturnType<typeof changeThemeId>
+
+export const changeThemeId = (id: number) => {
+  return {
+    type: 'SET_THEME_ID',
+    id
+  } as const
+}
